9685ddcd868c48756baf5fb17aa8c4e167d4a904
SubhashiniGajendran <subhashini.gajendran@gmail.com>
2025-11-21 09:40:37 +0530

    first commit

---
+++ src/datatype/var.ts
@@ -0,0 +1,6 @@
+let age:number = 25;
+let userName:string = "John Doe";
+let isLoggedIn:boolean = true;
+let scores:number[] = [90, 85, 88];
+let user: { id: number; name: string } = { id: 1, name: "Alice" };
+console.log(age, userName, isLoggedIn, scores, user);
